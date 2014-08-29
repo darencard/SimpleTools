823442019fb7fd1fc0688a33e1d47662b11ff889
darencard <dcard@uta.edu>
2014-08-28 19:44:57 -0500

    Initial commit

---
+++ delimiter_swap.py
@@ -0,0 +1,58 @@
+#!/usr/local/env python
+
+#print __name__
+
+import os
+import optparse
+
+usage_line = """
+delimiter_swap.py
+
+Version 1.0 (28 August, 2014)
+License: GNU GPLv2
+To report bugs or errors, please contact Daren Card (dcard@uta.edu).
+This script is provided as-is, with no support and no guarantee of proper or desirable functioning.
+
+A simple script that converts comma-separated value (csv) files into tab-separated value (tsv) files. \
+Uses simple sed find-and-replace unix command.
+
+python delimiter_swap.py -i <input> -o <output> -t <csv/tsv>"""
+
+#################################################
+###           Parse command options           ###
+#################################################
+
+usage = usage_line
+                        
+parser = optparse.OptionParser(usage=usage)
+parser.add_option("-i", action="store", type = "string", dest = "input", help = "input file")
+parser.add_option("-o", action="store", type = "string", dest = "output", help = "output file")
+parser.add_option("-t", action="store", type = "string", dest = "type", help = "input file type (csv or tsv)")
+
+options, args = parser.parse_args()
+
+
+#################################################
+###           Convert CSV to TSV	          ###
+#################################################
+
+def csv2tsv():
+	if options.input is None:
+		print "\n***Error: specify input file!***\n"
+	elif options.output is None:
+		print "\n***Error: specify output file!***\n"
+	elif options.type is None:
+		print "\n***Error: specify input file type (csv or tsv)!***\n"
+	else:
+		if options.type == "csv":
+			print "\n***Converting from comma-separated to tab-separated***\n"
+			os.system("sed 's/,/\t/g' "+options.input+" > "+options.output)
+		elif options.type == "tsv":
+			print "\n***Converting from tab-separated to comma-separated***\n"
+			os.system("sed 's/\t/,/g' "+options.input+" > "+options.output)
+		else:
+			print """\n***Error: file type must be "csv" or "tsv"!***\n"""
+	
+		
+csv2tsv()
+	
---
+++ remove_duplicate_rows.py
@@ -0,0 +1,58 @@
+#!/usr/local/env python
+
+#print __name__
+
+import os
+import optparse
+
+usage_line = """
+remove_duplicate_rows.py
+
+Version 1.0 (28 August, 2014)
+License: GNU GPLv2
+To report bugs or errors, please contact Daren Card (dcard@uta.edu).
+This script is provided as-is, with no support and no guarantee of proper or desirable functioning.
+
+A simple script that removes duplicate rows based upon a designated column number (1, 2, ..., n). \
+Passing the designated column option of 0 will filter rows that are completely identical (i.e., \
+not by column). Uses a Unix awk command and both tab separated value (tsv) and comma separated \
+value (csv) files are accepted.
+
+python remove_duplicate_rows.py -i <input_csv> -o <output_tsv> -c <0,1,2...>"""
+
+#################################################
+###           Parse command options           ###
+#################################################
+
+usage = usage_line
+                        
+parser = optparse.OptionParser(usage=usage)
+parser.add_option("-i", action="store", type = "string", dest = "input", help = "input file")
+parser.add_option("-o", action="store", type = "string", dest = "output", help = "output file")
+parser.add_option("-c", action="store", type = "string", dest = "column", help = "column to use when filtering duplicates")
+
+options, args = parser.parse_args()
+
+
+#################################################
+###       Filter by specified column	      ###
+#################################################
+
+def rmdups():
+	if options.input is None: 
+		print "\n***Error: specify input file!***\n"
+	elif options.output is None:
+		print "\n***Error: specify output files!***\n"
+	elif options.column is None:
+		print "\n***Error: specify column to use for filtering duplicates!***\n"
+	else:
+		if options.column == "0":
+			column = "entire row (or all columns)"
+		else:
+			column = "column "+options.column
+		print "\n***Removing duplicate rows based upon "+str(column)+"***\n"
+		line = str("awk '{ if ($"+options.column+" in stored_lines) x="+options.column+"; else print; stored_lines[$"+options.column+"]="+options.column+" }' "+options.input+" > "+options.output)
+		os.system(line)
+		
+rmdups()
+	
